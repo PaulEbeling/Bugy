8e91a0175a7c2f4f891c57e2b6d2ae5d6a8cfb5e
Felix <felix.leucker@alumni.fh-aachen.de>
2023-06-10 15:44:28 +0200

    minor changes but also getting controller input

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@
 #include <chrono>
 #include "adafruitmotorhat.h"
 #include <wiringPi.h>
-#include <xinput.h>
-#include <minwinbase.h>
-#include <winerror.h>
 #include "gyro.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <linux/joystick.h>
 
 #define motor_nr1 1
 #define motor_nr2 4
@@ -20,27 +20,34 @@
 #define trigger 4 //wiringpi port for trigger
 #define led 5 //wiringpi port for led
 #define limit 10 //limit, how near the border is allowed to be in cm
-#define correction 5 //the correction for driving straight
+#define correction 2 //the correction for driving straight
 
 AdafruitMotorHAT hat;
-std::shared_ptr<AdafruitDCMotor> motor;
-std::shared_ptr<AdafruitDCMotor> motor2;
+std::shared_ptr <AdafruitDCMotor> motor;
+std::shared_ptr <AdafruitDCMotor> motor2;
+int gamepad;
 
 void straight(double sec);
-void left(bool left = true);
+
+void left(bool left = true, float degree = 90);
+
 void rectangle();
+
 double distance();
-void init();
+
+int init();
+
 void bypass();
 
-/// Interrupt Routine for STRG-C
-void signalHandler(int signum)
-{
-    std::cout << "Strg-C Programmende" << std::endl;
-	// Beenden Sie hier bitte alle Verbindung zu den Sensoren etc.
+void controller();
 
-    motor->run (AdafruitDCMotor::kRelease);
-    motor2->run (AdafruitDCMotor::kRelease);
+/// Interrupt Routine for STRG-C
+void signalHandler(int signum) {
+    std::cout << "Strg-C Programmende" << std::endl;
+    // Beenden Sie hier bitte alle Verbindung zu den Sensoren etc.
+
+    motor->run(AdafruitDCMotor::kRelease);
+    motor2->run(AdafruitDCMotor::kRelease);
 
     exit(signum);
 }
@@ -49,7 +56,7 @@ void signalHandler(int signum)
  * Drives a straight line with object recognition
  * @param milli_sec Duration how long it should drive forward, in ms
  */
-void straight(double milli_sec){
+void straight(double milli_sec) {
     //initialize vectors for xyz for movement correction
     std::vector<double> xyz(3);
     std::vector<double> xyz2(3);
@@ -58,37 +65,49 @@ void straight(double milli_sec){
     xyz2[2] = 0;
 
     unsigned long pre_time = micros(), aft_time = micros(), last_time = 0; //initialize time
-    motor->run (AdafruitDCMotor::kForward);
-    motor2->run (AdafruitDCMotor::kBackward);
+    motor->run(AdafruitDCMotor::kForward);
+    motor2->run(AdafruitDCMotor::kBackward);
     double time = millis() + milli_sec;
 
-    while(time - millis() > 0){
+    double z = 0;
+    while (time - millis() > 0) {
         xyz = get_xyz();
-        if(distance() <= limit) {
-            motor->run (AdafruitDCMotor::kRelease);
-            motor2->run (AdafruitDCMotor::kRelease);
+        if (distance() <= limit) {
+            motor->run(AdafruitDCMotor::kRelease);
+            motor2->run(AdafruitDCMotor::kRelease);
             bypass();
+            motor->run(AdafruitDCMotor::kForward);
+            motor2->run(AdafruitDCMotor::kBackward);
         }
 
-        if(z > correction || z < -correction){
+        if (z > correction || z < -correction) {
+            motor->run(AdafruitDCMotor::kRelease);
+            motor2->run(AdafruitDCMotor::kRelease);
 
+            z > correction ? motor->run(AdafruitDCMotor::kForward) :
+            motor->run(AdafruitDCMotor::kBackward);
+            delay(10);
+
+            motor->run(AdafruitDCMotor::kForward);
+            motor2->run(AdafruitDCMotor::kBackward);
         }
         z = z + ((last_time) * (xyz2[2] + xyz[2])) / (2000000); //calculate angle like in left()
+        //std::cout << 0 << "  " << 0 << "  " << z << "  " << last_time << "\n";
         aft_time = micros(); //stop "timer"
         last_time = aft_time - pre_time; // calculate time
         pre_time = micros(); //start "timer"
         xyz2 = get_xyz();
     }
 
-    motor->run (AdafruitDCMotor::kRelease);
-    motor2->run (AdafruitDCMotor::kRelease);
+    motor->run(AdafruitDCMotor::kRelease);
+    motor2->run(AdafruitDCMotor::kRelease);
 }
 
 /**
  * Dirves an 90 degree angle
  * @param left true it drives left round; false it is moving right round
  */
-void left(bool left){
+void left(bool left, float degree) {
     //initialize vectors for xyz
     std::vector<double> xyz(3);
     std::vector<double> xyz2(3);
@@ -100,7 +119,7 @@ void left(bool left){
     motor->run(left ? AdafruitDCMotor::kBackward : AdafruitDCMotor::kForward);
 
     //run motors until 84 degrees are reached; 84 -> because it works better, motors need timer to stop
-    for(double/* x = 0, y = 0,*/ z = 0; z < 84 && z > -84;) {
+    for (double/* x = 0, y = 0,*/ z = 0; z < degree - 6 && z > -degree + 6;) {
         xyz = get_xyz();/*
         x = x + ((last_time) * (xyz2[0] + xyz[0])) / (2000000);
         y = y + ((last_time) * (xyz2[1] + xyz[1])) / (2000000);*/
@@ -119,8 +138,8 @@ void left(bool left){
 /**
  * Drives a square with collision detection
  */
-void rectangle(){
-    for(int i = 0; i <= 4; i++){
+void rectangle() {
+    for (int i = 0; i <= 4; i++) {
         straight(3000);
         left();
     }
@@ -130,13 +149,13 @@ void rectangle(){
  * Measures the distance to an object
  * @return distance in cm
  */
-double distance(){
+double distance() {
     digitalWrite(trigger, HIGH);
     delay(0.01);
     digitalWrite(trigger, LOW);
-    while(digitalRead(echo) == LOW){}
+    while (digitalRead(echo) == LOW) {}
     double time = micros(); //start timer
-    while(digitalRead(echo) == HIGH){}
+    while (digitalRead(echo) == HIGH) {}
     time = micros() - time;
     //std::cout << time * 0.017  << "  " << time << "\n";
 
@@ -149,14 +168,14 @@ double distance(){
  * Initializes gyro Sensor and sets global variables for the motors and sets inputs/outputs for led and ultrasonic
  * blaster
  */
-void init(){
+int init() {
     gyro_init();
 
     //motor
     motor = hat.getMotor(motor_nr1);
     motor2 = hat.getMotor(motor_nr2);
-    motor->setSpeed (speed);
-    motor2->setSpeed (speed);
+    motor->setSpeed(speed);
+    motor2->setSpeed(speed);
 
     //ultrasonic blaster
     wiringPiSetup();
@@ -165,6 +184,16 @@ void init(){
 
     //led
     pinMode(led, OUTPUT);
+
+    //Controller
+    // Öffnen Sie das Gamepad-Gerät
+    gamepad = open("/dev/input/js0", O_RDONLY);
+
+    if (gamepad == -1) {
+        std::cerr << "Gamepad konnte nicht geöffnet werden." << std::endl;
+        return 1;
+    }
+    return 0;
 }
 
 /**
@@ -172,10 +201,10 @@ void init(){
  * to short it will go on with another bypass)
  * another bypass execution)then forward and
  */
-void bypass(){
+void bypass() {
     left();
     bool left_curve = true;
-    if(distance() < 30){
+    if (distance() < 30) {
         left(false);
         left(false);
         left_curve = false;
@@ -183,45 +212,69 @@ void bypass(){
 
     straight(4000);
     left_curve ? left(false) : left();
-    straight(4000);
+    straight(6000);
     left_curve ? left(false) : left();
     straight(4000);
     left_curve ? left() : left(false);
 
 }
 
-int main()
-{
+/**
+ * Tests for Controller inputs
+ */
+void controller() {
+    // Hauptprogrammschleife
+    while (true) {
+        // Lesen Sie die Gamepad-Ereignisse
+        struct js_event event;
+        ssize_t bytesRead = read(gamepad, &event, sizeof(event));
+
+        if (bytesRead == -1) {
+            std::cerr << "Fehler beim Lesen des Gamepad-Ereignisses." << std::endl;
+            break;
+        }
+
+        if (bytesRead == sizeof(event)) {
+            // Überprüfen Sie den Ereignistyp
+            if (event.type == JS_EVENT_AXIS) {
+                // Achsenwert erhalten
+                int axis = event.number;
+                int value = event.value;
+                std::cout << "Achse " << axis << " Wert: " << value << std::endl;
+
+                if (axis == 0) {
+                    straight(100);
+                }
+            } else if (event.type == JS_EVENT_BUTTON) {
+                // Knopfzustand erhalten
+                int button = event.number;
+                int value = event.value;
+                std::cout << "Button " << button << " gedrückt: " << value << std::endl;
+            }
+        }
+    }
+
+    // Schließen Sie das Gamepad-Gerät
+    close(gamepad);
+
+}
+
+
+int main() {
     // Csignal für Abbruch über STRG-C
     signal(SIGINT, signalHandler);
     init();
 
-    straight(100);
+    std::cout << "Please make Controller input";
+    controller();
+
+
+    straight(5000);
     std::cout << "Please make input.";
     char input;
-    for(;;) {
+    for (;;) {
         input = getchar();
-
-        DWORD dwResult;
-        for (DWORD i=0; i< XUSER_MAX_COUNT; i++ )
-        {
-            XINPUT_STATE state;
-            ZeroMemory( &state, sizeof(XINPUT_STATE) );
-
-            // Simply get the state of the controller from XInput.
-            dwResult = XInputGetState( i, &state );
-
-            if( dwResult == ERROR_SUCCESS )
-            {
-                // Controller is connected
-            }
-            else
-            {
-                // Controller is not connected
-            }
-        }
-
-        switch(input){
+        switch (input) {
             case 'w':
                 straight(100);
                 break;
